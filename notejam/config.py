4c2a3e923cb03ee2e1ac4910ffb37ae96f936045
Ajdin Kajdic <ajdin.kajdic@gmail.com>
2021-03-07 22:41:56 +0100

    Updated Config file

--- notejam/config.py
+++ notejam/config.py
@@ -2,6 +2,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 import urllib
 import pyodbc
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 
 class Config(object):
     DEBUG = True
@@ -9,16 +11,15 @@ class Config(object):
     SECRET_KEY = 'notejam-flask-secret-key'
     WTF_CSRF_ENABLED = True
     CSRF_SESSION_KEY = 'notejam-flask-secret-key'
-    DB_STRING = os.environ["DB_STRING"]
-    #params = urllib.parse.quote_plus("DRIVER={SQL Server Native Client 11.0};SERVER=notejam-db.database.windows.net;DATABASE=notejam-db;UID=notejam-sql;PWD=H5WAqjcN5N5Bcazf!")
-    #params = urllib.parse.quote_plus("DRIVER={ODBC Driver 17 for SQL Server};SERVER=notejam-db.database.windows.net;DATABASE=notejam-db;UID=notejam-sql;PWD=H5WAqjcN5N5Bcazf!")
-    params = urllib.parse.quote_plus(DB_STRING)
+    sql_database = os.environ['SQL_DATABASE']
+    sql_server = os.environ['SQL_SERVER']
+    sql_user = os.environ['SQL_USER']
+    sql_pwd = os.environ['SQL_PASSWORD']
+    params = urllib.parse.quote_plus("DRIVER={ODBC Driver 17 for SQL Server};SERVER=%s;DATABASE=%s;UID=%s;PWD=%s;" % (sql_server, sql_database, sql_user, sql_pwd,))
     SQLALCHEMY_DATABASE_URI = "mssql+pyodbc:///?odbc_connect=%s" % params
-    #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'notejam.db')
-
 
 class ProductionConfig(Config):
-    DEBUG = True
+    DEBUG = False
 
 
 class DevelopmentConfig(Config):
