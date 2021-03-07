d5e27780f5b817b7918710a6cb2970801c95256a
Ajdin Kajdic <ajdin.kajdic@gmail.com>
2021-03-07 16:50:38 +0100

    Updated config gile

--- notejam/config.py
+++ notejam/config.py
@@ -9,8 +9,10 @@ class Config(object):
     SECRET_KEY = 'notejam-flask-secret-key'
     WTF_CSRF_ENABLED = True
     CSRF_SESSION_KEY = 'notejam-flask-secret-key'
+    DB_STRING = os.environ["DB_STRING"]
     #params = urllib.parse.quote_plus("DRIVER={SQL Server Native Client 11.0};SERVER=notejam-db.database.windows.net;DATABASE=notejam-db;UID=notejam-sql;PWD=H5WAqjcN5N5Bcazf!")
-    params = urllib.parse.quote_plus("DRIVER={ODBC Driver 17 for SQL Server};SERVER=notejam-db.database.windows.net;DATABASE=notejam-db;UID=notejam-sql;PWD=H5WAqjcN5N5Bcazf!")
+    #params = urllib.parse.quote_plus("DRIVER={ODBC Driver 17 for SQL Server};SERVER=notejam-db.database.windows.net;DATABASE=notejam-db;UID=notejam-sql;PWD=H5WAqjcN5N5Bcazf!")
+    params = urllib.parse.quote_plus(DB_STRING)
     SQLALCHEMY_DATABASE_URI = "mssql+pyodbc:///?odbc_connect=%s" % params
     #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'notejam.db')
 
